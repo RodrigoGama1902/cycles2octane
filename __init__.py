dca215ecb8e447492024edd6a0656ec400b9b70e
RodrigoGama <rodrigo1902.rb@gmail.com>
2022-03-23 17:06:30 -0300

    Updated Add-on Version

--- __init__.py
+++ __init__.py
@@ -2,14 +2,16 @@ bl_info = {
     "name": "Cycles to Octane Converter",
     "description": "Node Tree Converter",
     "author": "Rodrigo Gama",
-    "version": (0, 1, 2),
-    "blender": (2, 93, 0),
+    "version": (0, 1, 3),
+    "blender": (3, 0, 1),
     "location": "View3D",
     "category": "3D View"}
+
 
 def register():
     from .addon.register import register_addon
     register_addon()
+
 
 def unregister():
     from .addon.register import unregister_addon
